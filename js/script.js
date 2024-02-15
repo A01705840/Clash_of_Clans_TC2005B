4e084a7bfc2413042ee02aaee256b9b321613c3b
Maria José Soto <A01705840@tec.mx>
2024-02-15 12:34:46 -0600

    clase de js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 alert('hola mundo');
+// console.info('Clahs of Clans');
+// console.error('Error');
+// console.warn('Advertencia');
+// console.assert(1 === 2, 'No son iguales');
+
+var precio = 100;
+console.log(precio);
+
+const nombre = prompt('Cual es tu nombre?');
+console.log("Hola "+nombre);
--- js/variables.js
+++ js/variables.js
@@ -1,1 +1,7 @@
 //Variables
+
+//Declaración de variables Antigua
+var luchadores = 20;
+//Declaración de variables Moderna
+const nombre = "Luchadores";
+let luchadoresPorEquipo = 5;
