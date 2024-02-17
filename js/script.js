23f3cd7c77f34ace50e01bba06c16b34e129e891
Maria José Soto <A01705840@tec.mx>
2024-02-16 19:48:57 -0600

    Lab 4 parte 5 y 6

--- js/script.js
+++ js/script.js
@@ -90,17 +90,175 @@ for (let i = 0; i < fila; i++){
     }
     document.write("</tr>");
 }
-/*1:
-Entrada: un número pedido con un prompt. Salida: Una tabla con los números del 1 al número dado con sus cuadrados y cubos. Utiliza document.write para producir la salida
+document.write("</table>");
 
-2:
-Entrada: Usando un prompt se pide el resultado de la suma de 2 números generados de manera aleatoria. Salida: La página debe indicar si el resultado fue correcto o incorrecto, y el tiempo que tardó el usuario en escribir la respuesta.
-3:
-Función: contador. Parámetros: Un arreglo de números. Regresa: La cantidad de números negativos en el arreglo, la cantidad de 0's, y la cantidad de valores mayores a 0 en el arreglo.
+//2:
+// Numeros aleatorios
+let num1 = Math.floor(Math.random() * 100);
+let num2 = Math.floor(Math.random() * 100);
+//Empieza el tiempo
+console.time('Execution Time');
+// comienza la tarea
+let guess = prompt("¿Cuánto es " + num1 + " + " + num2 + "?");
+// la tarea termina
+console.timeEnd('Execution Time')
+//Se imprime si el resultado es correcto o incorrecto
+if (guess == num1 + num2) {
+    console.log('Correcto');
+    document.write('Correcto' + "<br>");
+} else {
+    console.log('Incorrecto');
+    document.write('Incorrecto' + "<br>");
+}
+
+//3:
+//Meter los números separados por comas
+let input = prompt("Enter a list of numbers separated by commas:");
+//Separar los números
+let pre_array = (input.split(','));
+//Definir variables
+let negativos = 0;
+let ceros = 0;
+let positivos = 0;
+let array = [];
+//Convertir los string a numeros
+for(let i = 0; i < pre_array.length; i++){
+    array[i] = Number(pre_array[i]);
+    //Clasificar
+    if(array[i] < 0) {
+        negativos++;
+    }
+    if(array[i] == 0) {
+        ceros++;
+    }
+    if(array[i] > 0) {
+        positivos++;
+    }
+}
+//Imprimir
+document.write("Negativos: " + negativos + "<br>");
+document.write("Ceros: " + ceros + "<br>");
+document.write("Positivos: " + positivos + "<br>");
+console.log(array);
+
+// // 4:
+// //Meter los números separados por comas
+// let input_4 = prompt("Enter a list of numbers separated by commas for:");
+// //Separar los números
+// let pre_array_4 = (input.split(','));
+// //Definir variables
+// let array_4 = [];
+// //Convertir los string a numeros
+// for(let i = 0; i < pre_array_4.length; i++){
+//     array_4[i] = Number(pre_array_4[i]);
+// }
+
+
+//5:
+//Ingresar el número
+let numero = prompt("Ingrese un número");
+//Convertir el número a string
+let numero_string = numero.toString();
+//Invertir el string
+let numero_inverso = numero_string.split("").reverse().join("");
+//Imprimir el número invertido
+document.write("El número invertido es: " + numero_inverso + "<br>");
+
+//6:
+//Problema: Organizar los equipo de un torneo de futbol
+//Solución: Crear un objeto con los equipos y los juegos
+//Creamos el objeto de team, que tendrá los jugadores y los juegos
+const team = {
+    _players: [],
+    _games: [],
+    get players (){
+      return this._players
+    },
+    get games () {
+      return this._games
+    },
+    addPlayer(newFirstName,newLastName,newAge) {
+      let player = {
+        firstName: newFirstName,
+        lastName:newLastName,
+        age: newAge
+      };
+      this._players.push(player)
+    },
+    addGame(opponent,teamPoints,opponentPoints) {
+      let game = {
+        opponent: opponent,
+        teamPoints: teamPoints,
+        opponentPoints: opponentPoints
+      };
+      this._games.push(game)
+    },
+    did_we_won_season(_games) {
+        let wins = 0;
+        let losses = 0;
+        for(let i=0; i > _games.length; i++){
+            if(_games[i].teamPoints > _games[i].opponentPoints){
+                wins++;
+            }else{
+                losses++;
+            }
+            return wins, losses;
+        }
+        if(wins > losses){
+            document.write('¡¡¡¡¡¡We won the season!!!!!!'+ "<br>");
+            document.write('<img src="img/we_won_the_season.jpg">');
+        }
+
+    }
+  };
+  
+let player1 = {
+    firstName: 'Jerry',
+    lastName: 'Smith',
+    age: 30
+}
+let player2 = {
+    firstName: 'Tom',
+    lastName: 'Sanchez',
+    age: 30
+}
+let player3 = {
+    firstname: 'Jessica',
+    lastName: 'Johnass',
+    age: 30
+}
+
+let game1 = {
+opponent: 'Team 4',
+teamPoints: 3,
+opponentPoints: 1
+}
+let game2 = {
+opponent: 'Team 5',
+teamPoints: 0,
+opponentPoints: 0
+}
+let game3 = {
+opponent: 'Team 6',
+teamPoints: 0,
+opponentPoints: 0
+}
+const p_1 =  player1
+const p_2 = player2
+const p_3 = player3
+const g_1 = game1
+const g_2 = game2
+const g_3 = game3
+team._players.push(p_1,p_2,p_3)
+team._games.push(g_1,g_2,g_3)
+console.log(team._players)
+console.log(team._games)
+team.addPlayer('John','Carlson',33)
+team.addGame('Los Diablos',100,98)
+
+/*
 4:
 Función: promedios. Parámetros: Un arreglo de arreglos de números. Regresa: Un arreglo con los promedios de cada uno de los renglones de la matriz.
-5:
-Función: inverso. Parámetros: Un número. Regresa: El número con sus dígitos en orden inverso.
 6:
 Crea una solución para un problema de tu elección 
 (puede ser algo relacionado con tus intereses, alguna problemática que hayas identificado en algún ámbito, un problema de programación que hayas resuelto en otro lenguaje, un problema de la ACM, entre otros). El problema debe estar descrito en un documento HTML, y la solución implementada en JavaScript, utilizando al menos la creación de un objeto, el objeto además de su constructor deben tener al menos 2 métodos. Muestra los resultados en el documento HTML. */
