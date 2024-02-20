6b91bd1b29e6ae3bd147805fb97bc801b40baddb
Maria José Soto <A01705840@tec.mx>
2024-02-20 17:55:07 -0600

    poe.js cambios click

--- js/poe.js
+++ js/poe.js
@@ -1,6 +1,25 @@
-//Variables
+alert('hola mundo');
+const boton_add_do = document.getElementById("b-add_do");
+boton_add_do.onclick = () => {
+    console.log('click');
+}
+const boton_add_delegate = document.getElementById("b-add_delegate");
+boton_add_delegate.onclick = () => {
+    console.log('click');
+}
+const boton_add_schedule = document.getElementById("b-add_schedule");
+boton_add_schedule.onclick = () => {
+    console.log('click');
+}
+const boton_add_delete = document.getElementById("b-add_delete");
+boton_add_delete.onclick = () => {
+    console.log('click');
+}
 
-//Declaración de variables Antigua
-var luchadores = 20;
-//Declaración de variables Moderna
-let luchadoresPorEquipo = 5;
+//  let b_add = 0;
+//      for (let i; i < b_add; i++) {
+//          console.log("HOLA");
+//          document.write("HOLA");
+//          document.write("<input type='checkbox' id='cbox1del'/><input type='text' class='rounded-md bg-yellow-200 ring-1 shadow-sm w-40'/> <br></br>");
+//          b_add = 0;
+//      }
