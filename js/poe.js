199f8349c60fe69c6452e76980749568accce722
Maria José Soto <A01705840@tec.mx>
2024-02-20 20:34:54 -0600

    LAB 6 Completa

--- js/poe.js
+++ js/poe.js
@@ -1,25 +1,24 @@
 alert('hola mundo');
+
 const boton_add_do = document.getElementById("b-add_do");
+
 boton_add_do.onclick = () => {
-    console.log('click');
+    document.getElementById('cbox_hidden_do').hidden = false;
+    document.getElementById('in_hidden_do').hidden = false;
 }
+
 const boton_add_delegate = document.getElementById("b-add_delegate");
 boton_add_delegate.onclick = () => {
-    console.log('click');
+    document.getElementById('cbox_hidden_delegate').hidden = false;
+    document.getElementById('in_hidden_delegate').hidden = false;
 }
 const boton_add_schedule = document.getElementById("b-add_schedule");
 boton_add_schedule.onclick = () => {
-    console.log('click');
+    document.getElementById('cbox_hidden_schedule').hidden = false;
+    document.getElementById('in_hidden_schedule').hidden = false;
 }
 const boton_add_delete = document.getElementById("b-add_delete");
 boton_add_delete.onclick = () => {
-    console.log('click');
+    document.getElementById('cbox_hidden_delete').hidden = false;
+    document.getElementById('in_hidden_delete').hidden = false;
 }
-
-//  let b_add = 0;
-//      for (let i; i < b_add; i++) {
-//          console.log("HOLA");
-//          document.write("HOLA");
-//          document.write("<input type='checkbox' id='cbox1del'/><input type='text' class='rounded-md bg-yellow-200 ring-1 shadow-sm w-40'/> <br></br>");
-//          b_add = 0;
-//      }
