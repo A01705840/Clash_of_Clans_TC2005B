36455ef32bac6e23b4f98a5c7f55d9ba6e9fc620
Maria José Soto <A01705840@tec.mx>
2024-02-22 12:31:28 -0600

    arreglo

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 //fs es el módulo que contiene las funciones 
 //para manipular el sistema de archivos
 const filesystem = require('fs');
+const { setTimeout } = require('timers/promises');
 
 filesystem.writeFileSync('hola.txt', 'Hola mundo desde Node.js');
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 1000, 0, 120, 2000, 340, 1000];
+
+for (let item of arreglo){
+    setTimeout( () => {
+        console.log(item);
+    },   item);
+}
