40e9e8a9046cc959003263a07832345cdbdf7477
Maria José Soto <A01705840@tec.mx>
2024-02-23 21:25:54 -0600

    arreglar función de node.js que cambie y el profe me ayudo a cambiar :)

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
 //fs es el módulo que contiene las funciones 
 //para manipular el sistema de archivos
 const filesystem = require('fs');
-const { setTimeout } = require('timers/promises');
 
 filesystem.writeFileSync('hola.txt', 'Hola mundo desde Node.js');
 
+const te_hackie = () => {
+    console.log("jojo te hackié");
+}
+
+setTimeout(te_hackie, 7000);
+
 const arreglo = [5000, 60, 90, 100, 10, 20, 1000, 0, 120, 2000, 340, 1000];
 
-for (let item of arreglo){
-    setTimeout( () => {
+for (let item of arreglo) {
+    setTimeout(() => {
         console.log(item);
-    },   item);
+    }, item);
 }
+
+//Una función que recibi un arreglo de números y devuelvaso el promedio
+const arreglo_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let num_prom = 0;
+
+const promedio = (arreglo_1, num_prom) => {
+    for (let item of arreglo_1){
+        num_prom += item;
+    }
+    num_prom = num_prom / arreglo_1.length;
+    console.log(num_prom);
+}
+
+promedio(arreglo_1,num_prom);
+
+//Una función que reciba un string y escriba un string en un archivo de texto
+const escribir = (texto) => {
+    texto.writeFileSync('text.txt', texto);
+}
+
+
+
+
+const http = require('http');
+
+const server = http.createServer( (request, response) => {
+    console.log(request.url);
+    response.setHeader('Content-Type', 'text/html');
+    response.write("<!doctype html><html><head> <h1>Hola mundo</h1> </head><body><h2>Este es un servidor web</h2></body></html>");
+    response.end();
+});    
+
+
+server.listen(3000); //Puerto en el que escucha el servidor
