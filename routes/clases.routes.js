e9b5c8ded231e82cb5cb5283b3e5d62d2af5dec8
Maria José Soto <A01705840@tec.mx>
2024-03-04 12:44:44 -0600

    Añadir las preguntas respondidas

--- routes/clases.routes.js
+++ routes/clases.routes.js
@@ -24,3 +24,19 @@ module.exports = router;
 - Mustache
 - Nunjucks
 */
+
+//¿Qué beneficios encuentras en el estilo MVC?
+/*Los beneficios que encuentro en el estilo MVC son:
+- Separación de responsabilidades
+- Reutilización de código
+- Facilidad para el mantenimiento
+- Facilidad para la escalabilidad
+- Facilidad para la colaboración
+*/
+
+//¿Encuentras alguna desventaja en el estilo arquitectónico MVC?
+/*Algunas desventajas que encuentro en el estilo arquitectónico MVC son:
+- Complejidad: puede ser complejo de entender para principiantes
+- Dificultad para implementar: puede ser difícil implementar MVC en proyectos pequeños
+- Dificultad para depurar: si algo no funciona, puede ser difícil encontrar el problema 
+*/
