4c8ee4a9c39065d6dfc7c5a512aeaff0b00e673a
Maria José Soto <A01705840@tec.mx>
2024-03-04 12:42:30 -0600

    Añadir modelo de Libro

--- controllers/libros.controller.js
+++ controllers/libros.controller.js
@@ -1,32 +1,4 @@
-const libros = [{
-    nombre: "The Left Hand of Darkness", 
-    autor: "Ursula K. Le Guin", 
-    rating: 100, 
-    fecha: 1969, 
-    imagen: 'https://i.pinimg.com/564x/ae/8a/22/ae8a22093f108e52c1309e430edc3b67.jpg'
-    },
-    {
-    nombre: "I Have No Mouth, and I Must Scream", 
-    autor: "Harlan Ellison", 
-    rsting: 90, 
-    fecha: 1967,
-    imagen: 'https://i.pinimg.com/564x/01/7f/3d/017f3d4c4ee8ead03008a31dbdc71cbe.jpg'
-    }, 
-    {
-    nombre: "LAPVONA", 
-    autor: "Ottesa Moshfegh", 
-    rating: 90, 
-    fecha: 2022,
-    imagen: 'https://i.pinimg.com/564x/dc/2a/ef/dc2aef8de25e302f09f6f43157c5f441.jpg'
-    },
-    {
-    nombre: "La Muerte de Ivan Ilich", 
-    autor: "Leon Tolstoi", 
-    rating: 80, 
-    fecha: 1886,
-    imagen: 'https://i.pinimg.com/564x/62/d8/b1/62d8b1eeed930d7336965b5ad66cf898.jpg'
-    }
-];
+const Libro = require('../models/libros.model');
 
 exports.get_crear = (request, response, next) => {
     response.render('crear');
@@ -34,16 +6,17 @@ exports.get_crear = (request, response, next) => {
 
 exports.post_crear = (request, response, next) => {
     console.log(request.body);
-     libros.push({
-         nombre: request.body.nombre,
-         autor: request.body.autor,
-         rating: request.body.rating,
-         fecha: request.body.fecha,
-         imagen: request.body.imagen
-     });
+     const mis_libros = new Libro (
+         request.body.nombre,
+         request.body.autor,
+         request.body.rating,
+         request.body.fecha,
+         request.body.imagen
+     );
+     mis_libros.save();
      response.redirect('/');
 };
 
 exports.get_root = (request, response, next) => {
-    response.render('clases', {libros: libros});
+    response.render('clases', {libros: Libro.fetchAll(),});
 };
---
+++ models/libros.model.js
@@ -0,0 +1,58 @@
+
+const libros = [{
+    nombre: "The Left Hand of Darkness", 
+    autor: "Ursula K. Le Guin", 
+    rating: 100, 
+    fecha: 1969, 
+    imagen: 'https://i.pinimg.com/564x/ae/8a/22/ae8a22093f108e52c1309e430edc3b67.jpg'
+    },
+    {
+    nombre: "I Have No Mouth, and I Must Scream", 
+    autor: "Harlan Ellison", 
+    rsting: 90, 
+    fecha: 1967,
+    imagen: 'https://i.pinimg.com/564x/01/7f/3d/017f3d4c4ee8ead03008a31dbdc71cbe.jpg'
+    }, 
+    {
+    nombre: "LAPVONA", 
+    autor: "Ottesa Moshfegh", 
+    rating: 90, 
+    fecha: 2022,
+    imagen: 'https://i.pinimg.com/564x/dc/2a/ef/dc2aef8de25e302f09f6f43157c5f441.jpg'
+    },
+    {
+    nombre: "La Muerte de Ivan Ilich", 
+    autor: "Leon Tolstoi", 
+    rating: 80, 
+    fecha: 1886,
+    imagen: 'https://i.pinimg.com/564x/62/d8/b1/62d8b1eeed930d7336965b5ad66cf898.jpg'
+    }
+];
+
+module.exports = class Libro {
+    //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
+    contructur(mi_nombre, mi_autor, mi_rating, mi_fecha){
+        this.nombre = mi_nombre;
+        this.autor = mi_autor;
+        this.rating = mi_rating;
+        this.fecha = mi_fecha;
+        this.imagen = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9T4eZdmlbj1NKFcbcizY5I0AkfL6t0qaidw&usqp=CAU';
+    }
+
+    //Este método servirá para guardar de manera persistente el nuevo objeto. 
+    save() {
+    libros.push({
+        nombre: this.nombre,
+        autor: this.autor,
+        rating: this.rating,
+        fecha: this.fecha,
+        imagen: this.imagen
+        }); //Esta es la manera larga
+    //libros.push(this); //Agrega este objeto a la lista de libros
+    }
+//Métodos estáticos se ejecutan sobre la clase. No necesitan una instancia de la clase para ser ejecutados
+    static fetchAll() {
+        return libros;
+    }
+    
+}
