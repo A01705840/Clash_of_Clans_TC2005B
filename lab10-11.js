c4802e72b4adf19e71bd3136c4409feaa4ae5221
Maria José Soto <A01705840@tec.mx>
2024-03-05 14:03:34 -0600

    Arreglar formato de lab10-11

--- lab10-11.js
+++ lab10-11.js
@@ -1,34 +1,5 @@
 filesystem = require('fs');
 
-//Una función que recibi un arreglo de números y devuelvaso el promedio
-const arreglo_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let num_prom = 0;
-
-const promedio = (arreglo_1, num_prom) => {
-    for (let item of arreglo_1){
-        num_prom += item;
-    }
-    num_prom = num_prom / arreglo_1.length;
-    console.log(num_prom);
-}
-
-promedio(arreglo_1,num_prom);
-
-//Una función que reciba un string y escriba un string en un archivo de texto
-const escribir = (texto, nombreArchivo) => {
-    if (!nombreArchivo) {
-        nombreArchivo = 'archivotexto.txt';
-    }
-    filesystem.writeFile(nombreArchivo, texto, (error) => {
-        if (error) {
-            console.log('Error al escribir el archivo');
-        } else {
-            console.log('Archivo escrito correctamente');
-        }
-    });
-};
-
-
 const express = require('express');
 const app = express();
 
@@ -36,6 +7,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+//Middleware
 const session = require('express-session');
 app.use(session({
     secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
@@ -47,11 +19,9 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 var cookieParser = require('cookie-parser');
-
 app.use(cookieParser());
 
 const bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({extended: false}));
 
 //RUTAS
@@ -59,10 +29,7 @@ const rutasUsuarios = require('./routes/user.routes');
 app.use('/users', rutasUsuarios);
 
 const rutasClases = require('./routes/clases.routes');
-//Construir pagina principal de libros
 app.use('/', rutasClases);
-
-
 
 app.use((request, response, next) => {
   response.status(404);
